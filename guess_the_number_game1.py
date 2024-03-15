7b5351dd5c38c42353774d9f9c69b99b0b765fbd
takahashi-s1 <149812365+takahashi-s1@users.noreply.github.com>
2024-03-15 14:13:32 +0900

    Add files via upload

---
+++ guess_the_number_game1.py
@@ -0,0 +1,36 @@
+# guess the number game
+#コミットテスト
+import random
+import sys
+
+sys.stdout.buffer.write(b"Input your minimum number\n")
+sys.stdout.flush()
+n = sys.stdin.buffer.readline()
+
+sys.stdout.buffer.write(b"Input your maximum number\n")
+sys.stdout.flush()
+m = sys.stdin.buffer.readline()
+
+n_int = int(n.decode())
+m_int = int(m.decode())
+
+if (n_int > m_int):
+    sys.stdout.buffer.write(b"Minimum number is greater than maximum number\n")
+    sys.stdout.flush()
+else:
+    correctNumber = random.randint(n_int, m_int)
+    while True:
+        sys.stdout.buffer.write(b"guess the number\n")
+        sys.stdout.flush()
+        guess = sys.stdin.buffer.readline()
+        guess_int = int(guess.decode())
+        if guess_int == correctNumber:
+            sys.stdout.buffer.write(b"Correct\n")
+            sys.stdout.flush()
+            break
+        elif guess_int > correctNumber:
+            sys.stdout.buffer.write(b"Too high\n")
+            sys.stdout.flush()
+        else:
+            sys.stdout.buffer.write(b"Too low\n")
+            sys.stdout.flush()
